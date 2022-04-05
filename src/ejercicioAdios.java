aaebff714009d033ca8ec22c411b34f1862838af
borjaMontseny <borja.montseny@gmail.com>
2022-04-05 19:57:24 +0200

    Subiendo a través de Eclipse IDE.

---
+++ src/ejercicioAdios.java
@@ -0,0 +1,11 @@
+
+public class ejercicioAdios {
+
+	  public static void main(String[] args) {
+		    //System.out.println("Hola");
+		    System.out.println("Adios");
+		    System.out.println("FIN DE PROGRAMA");
+
+		  }
+
+		}
