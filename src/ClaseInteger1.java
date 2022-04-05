9c3939e9f28ddbe0937886826e0a8debbd1d9445
borjaMontseny <borja.montseny@gmail.com>
2022-04-05 20:15:47 +0200

    Cambio de prueba

--- src/ClaseInteger1.java
+++ src/ClaseInteger1.java
@@ -11,7 +11,7 @@ public class ClaseInteger1 {
 		
 		//Obtención del entero que almacena cada objeto Integer
 		//Si no se hace esto. Las líneas siguientes causarían error
-		//de compilación
+		//de compilación icariana
 		int n1=num1.intValue();
 		int n2=num2.intValue();
 		System.out.println("Suma de " + n1 + " y " + n2 + " vale " +(n1+n2));
